21e6b46926b0538322efe771ec07147a3e117d2a
TUSKIMEN <matheushenrique91737@gmail.com>
2025-03-14 06:23:59 +0000

    desafio novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
     char pais;
-    char estado;
+    char estado[5];
     char codigo[5];
     char cidade [10];
     int populacao;
@@ -10,7 +10,7 @@ int main(){
     int pontos;
 
     printf("digite o estado: \n");
-    scanf("%s",&estado);
+    scanf("%s", &estado);
 
     printf("digite o codigo: \n");
     scanf("%s", &codigo);
@@ -28,12 +28,62 @@ int main(){
     scanf("%d", &PIB);
 
     printf("digites pontos turisticos: \n");
-    scanf("%s", &pontos);
+    scanf("%d", &pontos);
 
-    printf("cidade: %s - Populacao: %d \n", cidade, populacao);
-    printf("área: %f - PIB: %d \n",area, PIB);
-    printf("pontos: %s", pontos);
+    printf("estado: %s \n",estado);
+    printf("codigo: %s \n",codigo);
+    printf("cidade: %s \n",cidade);
+    printf("populacao: %d \n",populacao);
+    printf("area: %f \n",area);
+    printf("PIB: %d \n",PIB);
+    printf("pontos turisticos: %d \n",pontos);
 
+
+    char estado2[5];
+    char codigo2[5];
+    char cidade2 [10];
+    int populacao2;
+    float area2;
+    int PIB2;
+    int pontos2;
+
+    printf("digite o estado: \n");
+    scanf("%s",&estado2);
+
+    printf("digite o codigo: \n");
+    scanf("%s", &codigo2);
+
+    printf("digite sua cidade: \n");
+    scanf("%s", &cidade2);
+
+    printf("digite populacao: \n");
+    scanf("%d", &populacao2);
+    
+    printf("digite area: \n");
+    scanf("%f", &area2);
+
+    printf("digite PIB: \n");
+    scanf("%d", &PIB2);
+
+    printf("digites pontos turisticos: \n");
+    scanf("%d", &pontos2);
+
+    printf("estado: %s \n",estado2);
+    printf("codigo: %s \n",codigo2);
+    printf("cidade: %s \n",cidade2);
+    printf("populacao: %d \n",populacao2);
+    printf("area: %f \n",area2);
+    printf("PIB: %d \n",PIB2);
+    printf("pontos turisticos: %d \n",pontos2);
+
+
+    
+    
+    return 0;
+
+    
+    
 }
+
 
  
